8d2c7dea878caa7e2a25cddbbbf97850c00417ed
Avijit <Avijit009>
2024-02-24 21:53:26 +0600

    Final Commit

---
+++ src/Components/Body/Gallery/imageData.js
@@ -0,0 +1,13 @@
+export const category = [
+    {
+        id: 4,
+        name: 'Car',
+        image: 'assets/images/category/car.jpg',
+    },
+    {
+        id: 5,
+        name: 'Luxury Car',
+        image: 'assets/images/category/luxury.jpg',
+    }
+
+]
